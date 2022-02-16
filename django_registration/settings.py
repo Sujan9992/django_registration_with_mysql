48233556d4cf601a38abc6a154da6daa7a1f1eda
Sujan Thapa <suznthapa9992@gmail.com>
2022-02-16 21:28:42 +0545

    Update settings.py

--- django_registration/settings.py
+++ django_registration/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pd-$)o$gxx_2%lm+w1$n@0$&mcx2&3x5(#xg)0g6*r*^lh@ili'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
